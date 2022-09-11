22a660fa7c6d415a5c314c10f1f30e474c5300f5
Vl-VSV <vvs_2015@icloud.com>
2022-09-11 13:48:31 +0300

    Initial Commit

---
+++ To Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do
+//
+//  Created by Vlad V on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
